75387b10906d6bba60e4e948d575dc126afac384
emiliano <emicabanillas.cab@gmail.com>
2022-08-17 15:35:21 -0300

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import icon from './pokeball.png'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  return(
+    <div className="">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <img src={icon} alt="" className="img-icon" />
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarColor02">
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="#">PokeMerch</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Nosotros</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-sm-2" type="text"/>
+        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Buscar</button>
+      </form>
     </div>
-  );
+  </div>
+</nav>
+
+    </div>
+  )
 }
 
 export default App;
